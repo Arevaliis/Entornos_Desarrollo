d085774d26a1cb968096fa197d89893a95c3292e
Jose Iglesias <joseiglesiasarevalo@gmail.com>
2025-04-03 10:52:34 +0200

    Merge

--- Hello_Git.py
+++ Hello_Git.py
@@ -1,3 +1,7 @@
 print("Este es un mensaje de prueba")
 print("Funcionalidad Sumar")
 print("Nueva funcionalidad resta")
+print("Nueva funcionalidad")
+
+print("Funcionalidad Multiplicacion")
+print("Funcionalidad Division")
