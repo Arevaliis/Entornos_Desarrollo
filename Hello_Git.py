79789e78b68c9701d2ceec3598c22077ea63df59
Jose Iglesias <joseiglesiasarevalo@gmail.com>
2025-04-30 10:28:39 +0200

    Revert "Borramos codigo para hacer revert"
    This reverts commit 6ea4b275a888692ae935e98ac914052e92195cc6.
    
    Revertimos cambios del anterior commit

--- Hello_Git.py
+++ Hello_Git.py
@@ -24,3 +24,9 @@ class Calculadora():
         return x**2
 
 calculadora = Calculadora()
+calculadora.raiz(int(input("numero")))
+calculadora.raiz(int(input("numero")))
+calculadora.raiz(int(input("numero")))
+calculadora.raiz(int(input("numero")))
+calculadora.raiz(int(input("numero")))
+calculadora.raiz(int(input("numero")))
